9ceddcfb3065fdb144a9c311e7e8365e59c1be11
Hans Hoffman <hoffman.hans.j@gmail.com>
2021-06-16 20:03:54 -0400

    chore: remove unused alias

--- priv/repo/migrations/20210616215004_create_hits.exs
+++ priv/repo/migrations/20210616215004_create_hits.exs
@@ -1,8 +1,6 @@
 defmodule Bagheera.Repo.Migrations.CreateHits do
   use Ecto.Migration
 
-  alias Bagheera.Links.Hit
-
   def change do
     create table(:hits) do
       add :link_id, references(:links, on_delete: :delete_all), null: false
