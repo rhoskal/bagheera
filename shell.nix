69fd8e8452f26b553ea544069b6eadbc2028d290
Hans Hoffman <hoffman.hans.j@gmail.com>
2021-07-12 18:20:38 -0400

    localize mix in nix

--- shell.nix
+++ shell.nix
@@ -1,24 +1,31 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 with pkgs;
 
 let
   inherit (lib) optionals;
 
-  basePackages = [
-    beam.packages.erlangR24.elixir_1_12
-    git
-  ];
+  basePackages = [ beam.packages.erlangR24.elixir_1_12 elixir_ls git nixfmt ];
 
-  inputs = basePackages
-    ++ optionals stdenv.isLinux inotify-tools
+  inputs = basePackages ++ optionals stdenv.isLinux inotify-tools
     ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
       # file_system used by phoenix_live_reload
       CoreFoundation
       CoreServices
     ]);
-in
-  mkShell {
-    buildInputs = inputs;
-  }
+
+  hooks = ''
+    mkdir -p .nix-mix
+    mkdir -p .nix-hex
+    export MIX_HOME=$PWD/.nix-mix
+    export HEX_HOME=$PWD/.nix-hex
+    export PATH=$MIX_HOME/bin:$PATH
+    export PATH=$HEX_HOME/bin:$PATH
+    export LANG=en_US.UTF-8
+    export ERL_AFLAGS="-kernel shell_history enabled"
+  '';
+in mkShell {
+  buildInputs = inputs;
+  shellHooks = hooks;
+}
 
