6861c87d4509c54f5aa2081e101060fdd3b227e1
Hans Hoffman <hoffman.hans.j@gmail.com>
2021-09-24 17:31:07 -0400

    refactor: credo fixes

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -17,6 +17,8 @@ defmodule BagheeraWeb.ConnCase do
 
   use ExUnit.CaseTemplate
 
+  alias Ecto.Adapters.SQL.Sandbox
+
   using do
     quote do
       # Import conveniences for testing with connections
@@ -32,10 +34,10 @@ defmodule BagheeraWeb.ConnCase do
   end
 
   setup tags do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Bagheera.Repo)
+    :ok = Sandbox.checkout(Bagheera.Repo)
 
     unless tags[:async] do
-      Ecto.Adapters.SQL.Sandbox.mode(Bagheera.Repo, {:shared, self()})
+      Sandbox.mode(Bagheera.Repo, {:shared, self()})
     end
 
     {:ok, conn: Phoenix.ConnTest.build_conn()}
--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -16,6 +16,8 @@ defmodule Bagheera.DataCase do
 
   use ExUnit.CaseTemplate
 
+  alias Ecto.Adapters.SQL.Sandbox
+
   using do
     quote do
       alias Bagheera.Repo
@@ -28,10 +30,10 @@ defmodule Bagheera.DataCase do
   end
 
   setup tags do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Bagheera.Repo)
+    :ok = Sandbox.checkout(Bagheera.Repo)
 
     unless tags[:async] do
-      Ecto.Adapters.SQL.Sandbox.mode(Bagheera.Repo, {:shared, self()})
+      Sandbox.mode(Bagheera.Repo, {:shared, self()})
     end
 
     :ok
