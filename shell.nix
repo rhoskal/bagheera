528976b559aab001599700c9292ec559e3393a75
Hans Hoffman <hoffman.hans.j@gmail.com>
2022-03-10 09:31:16 -0700

    nix release update to 21.11

--- shell.nix
+++ shell.nix
@@ -4,7 +4,7 @@ let
 
   inherit (pkgs.lib) optionals;
 
-  basePackages = [ pkgs.elixir pkgs.elixir_ls pkgs.git pkgs.nixfmt ];
+  basePackages = [ pkgs.elixir pkgs.git pkgs.nixfmt ];
 
   inputs = basePackages ++ optionals pkgs.stdenv.isLinux pkgs.inotify-tools
     ++ optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [
